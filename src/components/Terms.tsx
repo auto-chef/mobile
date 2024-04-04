7d30d00634becf0d01f04b1eed5f2507a35a8281
ericknathan <e.nathan@tixit.com.br>
2024-04-04 09:25:29 -0300

    feat: create terms component

---
+++ src/components/Terms.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text } from "react-native";
+
+import { theme } from "@/styles";
+
+export function Terms() {
+  return (
+    <Text style={styles.terms}>
+      Ao continuar você aceita os <Text style={styles.link}>Termos de Uso</Text>{" "}
+      e <Text style={styles.link}>Política de Privacidade</Text> definidos pela
+      AutoChef.
+    </Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  terms: {
+    color: theme.zinc[500],
+    textAlign: "center",
+    marginTop: "auto",
+  },
+  link: {
+    textDecorationLine: "underline",
+    color: theme.zinc[500],
+  },
+});
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ export * from "./Button";
 export * from "./Header";
 export * from "./Input";
 export * from "./Link";
+export * from "./Terms";
 export * from "./Title";

cb865bd4e2a7da4b784ed191aae38883eff81019
ericknathan <erick.capito@hotmail.com>
2024-04-23 21:50:43 -0300

    feat: use gemini ai as conversational model

--- src/hooks/use-prompt.hook.ts
+++ src/hooks/use-prompt.hook.ts
@@ -1,5 +1,20 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import { useEffect, useState } from "react";
+
+import { GOOGLE_GEMINI_API_KEY } from "@env";
 import { useTextToSpeech } from "./use-text-to-speech.hook";
+
+const genAI = new GoogleGenerativeAI(GOOGLE_GEMINI_API_KEY);
+
+const basicVoices = {
+  WELCOME_WITHOUT_RESTAURANT:
+    "Olá, seja bem-vindo ao AutoChef. Antes de prosseguirmos, preciso confirmar alguns detalhes. Em qual restaurante você gostaria de fazer seu pedido?",
+  RESTAURANT_NOT_FOUND:
+    "Não foi possível encontrar o restaurante informado. Que tal indicar o AutoChef para ele?",
+  CANCEL_MESSAGE: "Certo, seu pedido foi cancelado. Muito obrigado!",
+  CONFIRM_ORDER: "Seu pedido está correto?",
+  ORDER_CONFIRMED: "Seu pedido foi confirmado. Obrigado por usar o AutoChef!",
+};
 
 type Prompt = {
   id: string;
@@ -10,13 +25,21 @@ type Prompt = {
 export type PromptStatus = "LISTENING" | "SPEAKING" | "THINKING";
 
 interface UsePrompt {
-  prompt: Prompt[];
   onFinish?: () => void;
 }
 
-export function usePrompt({ prompt, onFinish }: UsePrompt) {
-  const { startSpeak, stopSpeak, isSpeaking } = useTextToSpeech();
+export function usePrompt({ onFinish }: UsePrompt) {
+  const [prompt, setPrompt] = useState<Prompt[]>([
+    {
+      id: "first-message",
+      agent: "user",
+    },
+  ]);
   const [currentPromptStep, setCurrentPromptStep] = useState(0);
+
+  const { startSpeak, stopSpeak, isSpeaking } = useTextToSpeech();
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro-latest" });
+
   const promptStatus: PromptStatus =
     prompt[currentPromptStep]?.agent === "user"
       ? "LISTENING"
@@ -38,9 +61,81 @@ export function usePrompt({ prompt, onFinish }: UsePrompt) {
     }
   }, [currentPromptStep]);
 
-  function handleNextPrompt() {
+  async function handleNextPrompt(message: string) {
+    setPrompt((prompt) => [
+      ...prompt,
+      {
+        id: String(prompt.length),
+        agent: "user",
+        text: message,
+      },
+    ]);
+
+    const nextMessage = await getNextAIMessage(message);
+
+    setPrompt((prompt) => [
+      ...prompt,
+      {
+        id: String(prompt.length),
+        agent: "ai",
+        text: Object.keys(basicVoices).includes(nextMessage)
+          ? basicVoices[nextMessage]
+          : nextMessage,
+      },
+    ]);
+
     setCurrentPromptStep((prev) => prev + 1);
   }
+
+  async function getNextAIMessage(message: string) {
+    try {
+      const defaultMessagesList = Object.entries(basicVoices)
+        .map(([name, value]) => `${name}: ${value}`)
+        .join(", ");
+
+      console.log({ defaultMessagesList });
+
+      const chat = model.startChat({
+        history: [
+          {
+            role: "user",
+            parts: [
+              {
+                text: `Você é uma inteligencia conversacional de voz chamada ChefIA no aplicativo AutoChef, que visa melhorar a experiência em drive thrus, permitindo fazer pedidos previamente. Você deve auxiliar o usuário a gerar o pedido. Caso o usuário não informe, solicite o nome do restaurante, os itens dos pedidos e pergunte se ele gostaria de remover algum extra do produto. Seja breve e considere que você está falando por voz. Apenas ao final do pedido, você deve me retorná-lo como uma mensagem semelhante a um JSON na mensagem como o seguinte exemplo, ele não precisa ser formatado com tabulações ou nada semelhante, me entregue um texto corrido: { restaurant: string; items: { name: string; amount: number; extras: { name: 'string; amount: number; type: 'add' | 'remove'; }}[] }. Além disso, a aplicação já terá algumas falas pré-gravadas que vão ser representadas como enums, elas são, quando houver algum caso semelhante, me retorne apenas o nome da enum: ${defaultMessagesList}. Os restaurantes válidos são apenas: 'Mc Donalds Av. Paulista' e 'Divino Fogão Shopping Cidade Jardim'. Lembre-se de confirmar o nome do restaurante com o usuário, além disso os itens do pedido ao final.`,
+              },
+            ],
+          },
+          {
+            role: "model",
+            parts: [{ text: "Entendido!" }],
+          },
+          ...prompt.slice(0, -1).map((prompt) => ({
+            role: prompt.agent === "ai" ? "model" : "user",
+            parts: [{ text: prompt.text }],
+          })),
+        ],
+        generationConfig: {
+          maxOutputTokens: 8192,
+          temperature: 1,
+          topP: 0.95,
+          topK: 40,
+        },
+      });
+
+      console.log({ chat, message });
+
+      const result = await chat.sendMessage(message);
+
+      console.log({ result });
+      console.log({ text: result.response.text() });
+
+      return result.response.text();
+    } catch (error) {
+      console.log("Erro na transcrição!", error);
+    }
+  }
+
+  console.log(prompt);
 
   return {
     promptStatus,
--- src/screens/modals/ChefIAModal.tsx
+++ src/screens/modals/ChefIAModal.tsx
@@ -9,49 +9,6 @@ import { theme } from "@/styles";
 export function ChefIAModal() {
   const { goBack } = useNavigation();
   const { handleStopPrompt, handleNextPrompt, promptStatus } = usePrompt({
-    prompt: [
-      {
-        id: "confirm-restaurant",
-        agent: "ai",
-        text: "Certo! Vamos confirmar seu pedido no Méqui Donalds Av. Paulista. O que você gostaria de pedir?",
-      },
-      {
-        id: "order",
-        agent: "user",
-      },
-      {
-        id: "ask-extras",
-        agent: "ai",
-        text: "Entendido. Você gostaria de adicionar uma anotação extra ao seu pedido? Tal como remoção ou adição de algum ingrediente?",
-      },
-      {
-        id: "extras",
-        agent: "user",
-      },
-      {
-        id: "ask-more",
-        agent: "ai",
-        text: "Certo. Mais algum item? Que tal acrescentar um McShake de morango?",
-      },
-      {
-        id: "more",
-        agent: "user",
-      },
-      {
-        id: "confirm-order",
-        agent: "ai",
-        text: "Seu pedido está quase pronto! Gostaria de confirmar o pedido?",
-      },
-      {
-        id: "confirm",
-        agent: "user",
-      },
-      {
-        id: "order-confirmed",
-        agent: "ai",
-        text: "Seu pedido foi confirmado! Obrigado por escolher o Méqui Donalds Av. Paulista.",
-      },
-    ],
     onFinish: onClose,
   });
 

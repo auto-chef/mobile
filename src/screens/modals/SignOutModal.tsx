ad14120988ad195e6680b585c5ebd33e572dfa1a
ericknathan <erick.capito@hotmail.com>
2024-05-19 20:47:14 -0300

    feat: signout on click on signout button

--- src/screens/modals/SignOutModal.tsx
+++ src/screens/modals/SignOutModal.tsx
@@ -1,18 +1,23 @@
 import { ConfirmationModal } from "@/components";
+import { useAuth } from "@/hooks";
 import { theme } from "@/styles";
 
 export function SignOutModal({ navigation }) {
+  const { signOut } = useAuth();
+
   return (
     <ConfirmationModal
       cancelText="Não, cancelar"
       confirmColor={theme.red[600]}
       confirmText="Sim, sair"
-      onConfirm={() =>
+      onConfirm={async () => {
+        await signOut();
+
         navigation.reset({
           index: 0,
           routes: [{ name: "Welcome" }],
         })
-      }
+      }}
       text="Deseja sair da sua conta?"
     />
   );

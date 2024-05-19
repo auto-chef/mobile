5b6d0fe239762607837c3a86e90217afb2603f57
ericknathan <erick.capito@hotmail.com>
2024-05-19 18:37:08 -0300

    fix: api interceptors and url

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,23 +1,26 @@
+import { API_BASE_URL } from "@env";
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: API_BASE_URL,
 });
 
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (axios.isAxiosError(error)) {
+    if (isApiError(error)) {
       if (error.response?.status.toString().startsWith("5")) {
         throw new Error(
           "Ocorreu um erro no servidor, tente novamente mais tarde!"
         );
       }
-    } else {
-      throw new Error(
-        "Ocorreu um erro inesperado, tente novamente mais tarde!"
-      );
+
+      if (error.message.includes("Network Error")) {
+        throw new Error("Não foi possível se conectar ao servidor 😢");
+      }
     }
+
+    throw error;
   }
 );
 
